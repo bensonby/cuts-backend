9196afbe6b24602d92177f88d178e4cceac11912
bensonby <bensonby@gmail.com>
2020-10-04 14:50:36 +0800

    Setup auth0 API #7

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/public', function (Request $request) {
+  return response()->json(["message" => "Hello public"]);
+});
+
+Route::get('/private', function (Request $request) {
+  return response()->json(["message" => "Hello private"]);
+})->middleware('jwt');
