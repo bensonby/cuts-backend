2fc8766b89c4d1cca80ea93ec2284d3cdb58b039
bensonby <bensonby@gmail.com>
2020-10-04 23:36:46 +0800

    Revise users table migration #5

--- database/migrations/2020_10_02_232407_setup_users_table.php
+++ database/migrations/2020_10_02_232407_setup_users_table.php
@@ -14,11 +14,13 @@ class SetupUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('auth0_user_id');
-            $table->dropColumn('email');
+            $table->string('sub')->unique(); // auth0 user id
             $table->dropColumn('email_verified_at');
             $table->dropColumn('password');
         });
+
+        // from create_password_resets_table.php
+        Schema::dropIfExists('password_resets');
     }
 
     /**
@@ -29,10 +31,16 @@ class SetupUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('auth0_user_id');
-            $table->string('email')->unique();
+            $table->dropColumn('sub');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+        });
+
+        // from create_password_resets_table.php
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 }
