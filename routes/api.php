e057ed2a50a1ef9958608d0bf4b183ba04cf2e4d
bensonby <bensonby@gmail.com>
2020-11-01 13:23:17 +0800

    Create meta information API #21

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,63 @@ use App\Http\Controllers\CourseController;
 |
 */
 
+function get_almanac() {
+  $earliestYear = env('CUTS_DATA_EARLIEST_YEAR');
+  $latestYear = env('CUTS_DATA_LATEST_YEAR');
+  $result = [];
+  for ($year = $latestYear; $year >= $earliestYear; $year--) {
+    for ($term = 1; $term <= 2; $term++) {
+      $yearterm = $year . '_' . $term;
+      $termStartDate = env('CUTS_TERM_START_DATE_' . $yearterm , '');
+      if ($termStartDate == '') {
+        continue;
+      }
+      $termEndDate = env('CUTS_TERM_END_DATE_' . $yearterm);
+      $holidays = explode(',', env('CUTS_HOLIDAYS_' . $yearterm));
+      if (!array_key_exists($year . '', $result)) {
+        $result[$year . ''] = [];
+      }
+      $result[$year . ''][$term . ''] = [
+        'term_start_date' => $termStartDate,
+        'term_end_date' => $termEndDate,
+        'holidays' => $holidays,
+      ];
+    }
+  }
+  return $result;
+}
+
+function get_meta_info() {
+  return [
+    'data_last_updated' => env('CUTS_DATA_LAST_UPDATE'),
+    'latest_year' => intval(env('CUTS_DATA_LATEST_YEAR', 2020)),
+    'almanac_version' => env('CUTS_ALMANAC_VERSION'),
+    'almanac' => get_almanac(),
+    'notice' => explode('|', env('CUTS_NOTICE')),
+    'update_available_message' => env('CUTS_UPDATE_AVAILABLE_MESSAGE'),
+    'is_update_available' => env('CUTS_IS_UPDATE_AVAILABLE'), // depreciated for ios_/android_latest_version
+    'latest_version' => env('CUTS_LATEST_VERSION'), // depreciated since android v4 / ios v1, for android_ and ios_
+    'ios_latest_version' => env('CUTS_IOS_LATEST_VERSION'),
+    'android_latest_version' => env('CUTS_ANDROID_LATEST_VERSION'),
+    'should_force_quit' => env('CUTS_SHOULD_FORCE_QUIT'),
+    'ios_update_link' => env('CUTS_IOS_UPDATE_LINK'),
+    'android_update_link' => env('CUTS_ANDROID_UPDATE_LINK'),
+    'tos_link' => env('CUTS_TOS_LINK'),
+    'privacy_policy_link' => env('CUTS_PRIVACY_POLICY_LINK'),
+  ];
+}
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/public', function (Request $request) {
-  return response()->json(["message" => "Hello public"]);
+Route::get('/app_meta_info.php', function (Request $request) {
+  return response()->json(get_meta_info());
 });
+
+Route::get('meta', function (Request $request) {
+  return response()->json(get_meta_info());
+})->middleware('version.check:5.0.0-dev,2.0.0-dev');
 
 Route::middleware('jwt')->group(function () {
   Route::get('/private', function (Request $request) {
