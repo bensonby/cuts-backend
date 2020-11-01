591cbb1a1be6500fdca0a3225984622c4efa958c
bensonby <bensonby@gmail.com>
2020-11-02 00:58:27 +0800

    Prevent redeclaring function #21

--- config/meta.php
+++ config/meta.php
@@ -1,29 +1,32 @@
 <?php
 
-function get_almanac() {
-  $earliestYear = intval(env('CUTS_DATA_EARLIEST_YEAR'));
-  $latestYear = intval(env('CUTS_DATA_LATEST_YEAR'));
-  $result = [];
-  for ($year = $latestYear; $year >= $earliestYear; $year--) {
-    for ($term = 1; $term <= 2; $term++) {
-      $yearterm = $year . '_' . $term;
-      $termStartDate = env('CUTS_TERM_START_DATE_' . $yearterm , '');
-      if ($termStartDate == '') {
-        continue;
+// prevent PHP Fatal Error for redeclaring the function
+if (!function_exists('get_almanac')) {
+  function get_almanac() {
+    $earliestYear = intval(env('CUTS_DATA_EARLIEST_YEAR'));
+    $latestYear = intval(env('CUTS_DATA_LATEST_YEAR'));
+    $result = [];
+    for ($year = $latestYear; $year >= $earliestYear; $year--) {
+      for ($term = 1; $term <= 2; $term++) {
+        $yearterm = $year . '_' . $term;
+        $termStartDate = env('CUTS_TERM_START_DATE_' . $yearterm , '');
+        if ($termStartDate == '') {
+          continue;
+        }
+        $termEndDate = env('CUTS_TERM_END_DATE_' . $yearterm);
+        $holidays = explode(',', env('CUTS_HOLIDAYS_' . $yearterm));
+        if (!array_key_exists($year . '', $result)) {
+          $result[$year . ''] = [];
+        }
+        $result[$year . ''][$term . ''] = [
+          'term_start_date' => $termStartDate,
+          'term_end_date' => $termEndDate,
+          'holidays' => $holidays,
+        ];
       }
-      $termEndDate = env('CUTS_TERM_END_DATE_' . $yearterm);
-      $holidays = explode(',', env('CUTS_HOLIDAYS_' . $yearterm));
-      if (!array_key_exists($year . '', $result)) {
-        $result[$year . ''] = [];
-      }
-      $result[$year . ''][$term . ''] = [
-        'term_start_date' => $termStartDate,
-        'term_end_date' => $termEndDate,
-        'holidays' => $holidays,
-      ];
     }
+    return $result;
   }
-  return $result;
 }
 
 return [
