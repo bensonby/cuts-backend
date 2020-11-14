bef3d281d0953132ac86b3cc48325bbd7669085f
bensonby <bensonby@gmail.com>
2020-11-14 21:46:50 +0800

    Setup basic index and tos and privacy page #29 #28

---
+++ resources/views/index.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.app')
+
+@section('title', 'CUTS App - CUHK Timetable System')
+
+@section('head')
+  <!-- Custom styles for this template -->
+  <link href="css/landing-page.min.css" rel="stylesheet">
+@endsection
+
+@section('content')
+  <!-- Navigation -->
+  <nav class="navbar navbar-light bg-light static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img class="align-middle" src="img/logo.png" style="width: 29; height: 29;" alt="CUTS logo" />
+        <span class="align-middle">CUTS (CUHK Timetable System)</span>
+      </a>
+      </a>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5">Plan, View and Organize Your CUHK Timetable with CUTS App!</h1>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form>
+            <div class="form-row">
+              <div class="col-12 col-md-6">
+                <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
+                  <a href='https://apps.apple.com/us/app/cuts-cuhk-timetable-system/id1498369252?mt=8' class="align-middle" style="display: inline-block;"><img alt='Get it on App Store' src='img/ios-badge.png' class="align-middle" style="width: 209px; height: 62px;"/></a>
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <a href='https://play.google.com/store/apps/details?id=com.cuhkcuts&hl=en_US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' style="width: 240px;"/></a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Icons Grid -->
+  <section class="features-icons bg-light text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-screen-desktop m-auto text-primary"></i>
+            </div>
+            <h3>Intuitive Interface</h3>
+            <p class="lead mb-0">You will surely love the timetable planning experience in CUTS.</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-layers m-auto text-primary"></i>
+            </div>
+            <h3>Versatile Scheduler</h3>
+            <p class="lead mb-0">CUTS can show your next lesson and put your schedule to your calendar.</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-check m-auto text-primary"></i>
+            </div>
+            <h3>No annoying wait</h3>
+            <p class="lead mb-0">Free of ads and splash screen. View your timetable in 1 second.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+          <ul class="list-inline mb-2">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/cuhk.cuts">Contact</a>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+            <li class="list-inline-item">
+              <a href="https://cuts.oursky.com/tos.html">Terms of Use</a>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+            <li class="list-inline-item">
+              <a href="https://cuts.oursky.com/privacy-policy.html">Privacy Policy</a>
+            </li>
+          </ul>
+          <p class="text-muted small mb-4 mb-lg-0">&copy; CUTS 2020. All Rights Reserved.</p>
+        </div>
+        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item mr-3">
+              <a href="https://www.facebook.com/cuhk.cuts" style="color: #4267b2;">
+                <i class="fab fa-facebook fa-2x fa-fw"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+@endsection
---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,159 @@
+@extends('layouts.app')
+
+@section('title', 'Privacy Policy - CUTS')
+
+@section('head')
+
+@section('content')
+  <h1>Privacy Policy</h1>
+  <i>Version 2.0 of This Agreement was created on August 1, 2020.</i>
+  <p>
+    CUTS (CUHK Timetable System) provide timetable-related utilities for CUHK undergraduates.
+    CUTS is committed to protecting and respecting your privacy.
+    This Privacy Policy (this “Policy”) sets out how we collect and process personal information about you
+    when you visit our website, use the Android App or iOS App (our "Services"), or when you otherwise do business or make contact with us.
+  </p>
+  <p>
+    The scope of this Policy is limited to the information collected or received by CUTS through your use of the Service.
+    CUTS is not responsible for the actions of third parties – people or companies – their use of information you provide to them, or any products or services they may offer.
+    By using the Service, you are expressing your agreement to this Policy and the processing of your data in the manner provided in this Policy.
+  </p>
+  <p>
+    Please read this policy carefully to understand how we handle and treat your personal information.
+  </p>
+  <h2>What information do we collect and how are they used?</h2>
+  <p>
+    CUTS collects data to enable us to operate the Services effectively,
+    and to provide you with the best experiences on our website and our Services.
+    You provide some of this data to us directly, such as when you register to use our Services, make an enquiry through our website, or contact us for support.
+    We get some of your data by recording how you interact with our website and our Services by, for example,
+    using technologies like cookies. We also obtain and process data in the context of providing the Services.
+  </p>
+  <h3>Timetable data</h3>
+  <p>
+    When using our services, we collect data about your interactions with our services.
+    The information we collect may include your user ID, timetable, selected courses and periods, search history
+    and/or other information that, we believe, is necessary to improve user/your experience and maintain the quality of our Service.
+  </p>
+  <h3>User Device and Location</h3>
+  <p>
+    This information is needed for us to provide troubleshooting solutions and to understand the usage trends of our users.
+    The information we collect may include device ID, hardware type, MAC address, IMEI, OS, device name, email address, location, IP, and/or other location-based information.
+  </p>
+  <p>
+    The data listed above can also be used to track the content you access in connection with the Service and your online behavior or to deliver, target and improve our Service.
+  </p>
+  <p>
+    With your full consent, we may collect and process information on your precise location.
+    We will use this information only to enhance the immediate user experience via functions like Nearby Friend finding.
+  </p>
+  <h3>Access to Calendar</h3>
+  <p>
+    Access to your device calendar is necessary when you opt to put your timetable into your device calendar.
+    Explicit request is shown for you to grant the access request for the App to access your device calendars.
+  </p>
+  <h3>Third-Party Services/SDKs</h3>
+  <p>
+    We use the following third party services and SDKs:
+  </p>
+  <ul>
+    <li>Sentry: <a target="_blank" href="https://www.sentry.io">https://www.sentry.io</a> - for app error monitoring</li>
+    <li>Auth0: <a target="_blank" href="https://auth0.com">https://auth0.com</a> - for authentication and authorization</li>
+  </ul>
+  <p>
+    All third party services/SDKs provide the same or equal protection of user data as stated in this privacy policy.
+  </p>
+  <p>
+    When you log in to our apps or websites using third-party login services (“Login Service”) such as Auth0, Facebook Login, Sign in with Apple,
+    the Login Services will provide you the option to share your personal information with us.
+    Your personal information we receive via Login Services may include your name, email address, user ID, city, state or country you live, sex, birth date, profile picture.
+    We may collect and record information through the Login Service in accordance with the policies and terms of that Login Service.
+  </p>
+  <p>
+    Information collected will be used to create your account, verify your identity when you log in, and allow you to use our Service.
+    If you want us to delete the data we receive from these Login Service about you, please contact us at bensonby@gmail.com.
+  </p>
+  <h3>User Support</h3>
+  <p>
+    We may need to collect your email address in order to respond when you contact us for support.
+    We may also collect your device information (such as your OS version and device model), location-based information, Player ID or device ID, and your service request information to provide proper solutions in troubleshooting situations.
+  </p>
+  <h3>Websites and Social Media Features</h3>
+  <p>
+    When you visit our sites, we may collect your personal information for the purpose of improving user experience on the sites.
+    Such information may include your device OS, location, browser type, language, referring web address, your usage of the site, gender, age, interests.
+  </p>
+  <p>
+    Our websites may include Social Media Features, such as the Facebook Like button and Widgets,
+    and Share button or interactive mini-programs that run on our site.
+    These Features may collect your IP address, the page you are visiting on our site,
+    and may set a cookie to enable the Feature to function properly.
+    Social Media Features and Widgets are either hosted by a third party or hosted directly on our Site.
+    Your interactions with these Features are governed by the Privacy Policies of the companies providing them.
+  </p> 
+  <h2>Do we disclose any information to outside parties?</h2>
+  <p>
+    We share your personal data with your consent, or as necessary to provide the Services to you.
+    Finally, we will access, transfer, disclose, and/or preserve personal data, when we have a good faith belief that doing so is necessary to:
+  </p>
+  <ul>
+    <li>
+      comply with applicable law or respond to valid legal process, judicial orders, or subpoenas;
+    </li>
+    <li>
+      respond to requests from public or governmental authorities, including for national security or law enforcement purposes;
+    </li>
+    <li>
+      protect the vital interests of our users, customers, or other third parties (including, for example, to prevent spam or attempts to defraud users of our products, or to help prevent the loss of life or serious injury of anyone);
+    </li>
+    <li>
+      operate and maintain the security of our Services, including to prevent or stop an attack on our computer systems or networks;
+    </li>
+    <li>
+      protect the rights, interests or property of Sentry or third parties;
+    </li>
+    <li>
+      prevent or investigate possible wrongdoing in connection with the Services; or
+    </li>
+    <li>
+      enforce our Terms of Service.
+    </li>
+  </ul>
+
+  <p>
+    We may use and share aggregated non-personal information with third parties for analytics purposes.
+  </p>
+  <p>
+    We do not sell or trade your personal information to third parties.
+  </p>
+  <h2>Data Retention</h2>
+  <p>
+    We may retain your personal information as long as you continue to use the Services,
+    have an account with us, or for as long as is necessary to fulfill the purposes outlined in the policy.
+    You can ask to close your account by contacting us at the details above,
+    and we will delete your personal information on request.
+  </p>
+  <p>
+    We may, however, retain personal information for an additional period as is permitted or required under applicable laws,
+    for legal, or regulatory reasons, or for legitimate and lawful business purposes.
+  </p>
+  <h2>Your Rights</h2>
+  <p>
+    You can at any time contact us at bensonby@gmail.com to get your personal data deleted.
+    You may also request to have your personal data exported.
+    If you have not registered any account in CUTS, you can simply uninstall the iOS/Android app
+    in order to remove any stored data.
+    If you need any further assistance regarding your rights, feel free to contact us at bensonby@gmail.com.
+  </p>
+  <h2>Changes to our Privacy Policy</h2>
+  <p>
+    We will update this privacy statement when necessary to reflect customer feedback and changes in our Services.
+    When we post changes to this statement, we will revise the “last updated” date at the top of the statement.
+    If there are material changes to the statement or in how Sentry will use your personal data,
+    we will notify you either by prominently posting a notice of such changes before they take effect or by directly sending you a notification.
+    We encourage you to periodically review this privacy statement to learn how Sentry is protecting your information.
+  </p>
+  <h2>Contact Us</h2>
+  <p>
+    If you have any questions or concerns about our Privacy Policy, please contact us at bensonby@gmail.com
+  </p>
