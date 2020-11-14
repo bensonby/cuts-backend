3189307bc8c5ee5b96f2959892b1bf89257507ce
bensonby <bensonby@gmail.com>
2020-11-14 21:46:50 +0800

    tmp web routes not removed welcome page

---
+++ resources/views/tos.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'Terms of Service - CUTS')
+
+@section('head')
+
+@section('content')
+  <h1>Terms of Service</h1>
+  <i>Version 2.0 of This Agreement was created on August 1, 2020.</i>
+
+<p>Access to and use of 'CUTS (CUHK Timetable System)' (abbreviated as 'CUTS' in below) is subjected to the following terms:</p>
+
+<ol>
+<li>By the continued use of CUTS you agree to be legally bound by these terms. If you do not agree to be legally bound by all the following terms please do not access and/or use CUTS.</li>
+<li>CUTS may change these terms at any time by posting changes online. Your continued use of CUTS after changes are posted means you agree to be legally bound by these terms as updated and/or amended.</li>
+<li>You may not copy, reproduce, republish, download, post, broadcast, transmit or otherwise use the content provided in CUTS in any way except for your own personal, non-commercial use.</li> 
+<li>You agree to use CUTS only for lawful purposes, and in a way that does not infringe the rights of, restrict or inhibit anyone else's use and enjoyment of the CUTS. Prohibited behaviour includes harassing or causing distress or inconvenience to any person, transmitting obscene or offensive content or disrupting the normal flow of dialogue within the CUTS.</li>
+<li>Not being an official application supported by CUHK, CUTS do not assure you for 100% up-to-date and correct information on the course information (including professor information as well). Please refer to the Registration and Examinations Section (RES) of CUHK for the confirmation of information (http://www.cuhk.edu.hk/rgs).</li>
+<li>CUTS will not be liable for any damages, including indirect or consequential damages, or any damages arising from use or loss of use, data or inaccurate information, whether in contract, negligence or other actions, arising from or in connection with the use of the CUTS.</li>
+<li>CUTS will not be responsible for the consequences of following any couse suggestion advice offered within the app.</li>
+<li>If any of these terms are determined to be illegal, invalid or otherwise unenforceable by reason of the laws of any state or country in which these terms are intended to be effective, then to the extent and within the jurisdiction in which that term is illegal, invalid or unenforceable, it shall be severed and deleted from these terms and the remaining terms shall survive, remain in full force and effect and continue to be binding and enforceable.</li>
+</ol>
+<p>CUTS (CUHK Timetable System) Facebook Application © 2020</p>
+  <h2>Contact Us</h2>
+  <p>
+    If you have any questions or concerns about our Terms of Service, please contact us at bensonby@gmail.com
+  </p>
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/tos.html', function () {
+    return view('tos');
+});
+
+Route::get('/privacy-policy.html', function () {
+    return view('privacy');
 });
